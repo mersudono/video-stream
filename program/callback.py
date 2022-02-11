95a3fdd34d246006180ff9fda152498dc7553c1f
mersudono <98972735+mersudono@users.noreply.github.com>
2022-02-11 13:51:54 +0330

    Update callback.py

--- program/callback.py
+++ program/callback.py
@@ -62,20 +62,15 @@ async def cbguides(_, query: CallbackQuery):
     await query.edit_message_text(
         f"""❓ How to use this Bot ?, read the Guide below !
 
-1.) First, add this bot to your Group.
-2.) Then, promote this bot as administrator on the Group also give all permissions except Anonymous admin.
-3.) After promoting this bot, type /reload in Group to update the admin data.
-3.) Invite @{ASSISTANT_NAME} to your group or type /userbotjoin to invite her (unfortunately the userbot will joined by itself when you type `/play (song name)` or `/vplay (song name)`).
-4.) Turn on/Start the video chat first before start to play video/music.
+1.) ابتدا ربات را به گروه خود اضافه کنید.
+2.) سپس، ربات را به عنوان مدیر ترفیع کنید و همه مجوزها را به جز ادمین ناشناس به آن بدهید.
+3.) پس از ارتقاء ربات، در گروه دستور /reload را تایپ کنید تا داده های مدیران به روز شود (پس از اضافه کردن مدیر جدید با دستور /reload قابلیت استفاده از ربات را به او بدهید).
+4.) یوزربات @{ASSISTANT_NAME} را به گروه خود اضافه کنید یا از دستور /userbotjoin استفاده کنید | اگر مراحل قبل را به درستی انجام دهید بعد از ( /play . . . ریپلی/نام ترَک) یوزربات خود به خود به گروه اضافه میشود!
+4.) قبل از شروع استریم ویس چت را باز کنید.
 
-`- END, EVERYTHING HAS BEEN SETUP -`
+`- و تمام، همه چیز با موفقیت پیش رفت!-`
 
-📌 If the userbot not joined to video chat, make sure if the video chat already turned on and the userbot in the chat.
-
-💡 If you have a follow-up questions about this bot, you can tell it on my support chat here: @{GROUP_SUPPORT}.""",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🔙 Go Back", callback_data="cbstart")]]
-        ),
+💡 **اگه سوالی داری میتونی اونو تو گروه پشتیبانی رفع کنی**: @{GROUP_SUPPORT}.
     )
 
 
@@ -91,12 +86,10 @@ async def cbcmds(_, query: CallbackQuery):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("👷🏻 Admin Cmd", callback_data="cbadmin"),
-                    InlineKeyboardButton("🧙🏻 Sudo Cmd", callback_data="cbsudo"),
+                    InlineKeyboardButton("👷🏻 دستورات ادمین", callback_data="cbadmin"),
+                    InlineKeyboardButton("🧙🏻 دستورات سودو", callback_data="cbsudo"),
                 ],[
-                    InlineKeyboardButton("📚 Basic Cmd", callback_data="cbbasic")
-                ],[
-                    InlineKeyboardButton("🔙 Go Back", callback_data="cbstart")
+                    InlineKeyboardButton("📚 دستورات پایه", callback_data="cbbasic")
                 ],
             ]
         ),
@@ -105,78 +98,60 @@ async def cbcmds(_, query: CallbackQuery):
 
 @Client.on_callback_query(filters.regex("cbbasic"))
 async def cbbasic(_, query: CallbackQuery):
-    await query.answer("basic commands")
+    await query.answer("دستورات پایه")
     await query.edit_message_text(
-        f"""🏮 here is the basic commands:
+        f"""🏮 لیست دستورات پایه ربات:
 
-» /play (song name/link) - play music on video chat
-» /vplay (video name/link) - play video on video chat
-» /vstream - play live video from yt live/m3u8
-» /playlist - show you the playlist
-» /video (query) - download video from youtube
-» /song (query) - download song from youtube
-» /lyric (query) - scrap the song lyric
-» /search (query) - search a youtube video link
+» /play (اسم ترَک/لینک/ریپلی) - پخش -آهنگ- در ویس چت
+» /vplay (اسم ویدیو/لینک/ریپلی) - پخش -ویدیو- در ویس چت
+» /playlist - نمایش پلی لیست گروه (صف)
+» /video (query) - دانلود ویدیو از یوتیوب
+» /song (query) - دانلود آهنگ از یوتیوب
+» /lyric (query) - دریافت متن آهنگ
+» /search (query) - جستجو لینک یوتیوب
 
-» /ping - show the bot ping status
-» /uptime - show the bot uptime status
-» /alive - show the bot alive info (in Group only)
+» /ping - نمایش وضعیت پینگ ربات
+» /uptime - نمایش وضعیت uptime ربات
+» /alive - نمایش وضعیت alive بات (صرفا در گروه)
 
 ⚡️ __Powered by {BOT_NAME} AI__""",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🔙 Go Back", callback_data="cbcmds")]]
-        ),
     )
 
 
 @Client.on_callback_query(filters.regex("cbadmin"))
 async def cbadmin(_, query: CallbackQuery):
-    await query.answer("admin commands")
+    await query.answer("دستورات ادمین")
     await query.edit_message_text(
-        f"""🏮 here is the admin commands:
+        f"""🏮 لیست دستورات ادمین (گروه):
 
-» /pause - pause the stream
-» /resume - resume the stream
-» /skip - switch to next stream
-» /stop - stop the streaming
-» /vmute - mute the userbot on voice chat
-» /vunmute - unmute the userbot on voice chat
-» /volume `1-200` - adjust the volume of music (userbot must be admin)
-» /reload - reload bot and refresh the admin data
-» /userbotjoin - invite the userbot to join group
-» /userbotleave - order userbot to leave from group
+» /pause - مکث یک ترَک
+» /resume - ادامه یک ترَک
+» /skip - تغییر|اسکیپ به ترَک بعدی
+» /stop - استریم را متوقف کنید
+» /vmute - یوزربات را در ویسچت میوت کنید
+» /vunmute - میوت بودن یوزربات را در ویسچت لغو کنید
+» /volume `1-200` - تغییر حجم صدای آهنگ در استریم (یوزربات باید ادمین باشد)
+» /reload - تازه سازی ربات و اطلاعات ادمین های گروه
+» /userbotjoin - دعوت یوربات به گروه
+» /userbotleave - خروج یوزربات از گروه
 
 ⚡️ __Powered by {BOT_NAME} AI__""",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🔙 Go Back", callback_data="cbcmds")]]
-        ),
     )
 
 @Client.on_callback_query(filters.regex("cbsudo"))
 async def cbsudo(_, query: CallbackQuery):
-    await query.answer("sudo commands")
+    await query.answer("دستورات سودو ها")
     await query.edit_message_text(
-        f"""🏮 here is the sudo commands:
+        f"""🏮 لیست دستورات سودو ها:
 
-» /gban (`username` or `user id`) - for global banned people
-» /ungban (`username` or `user id`) - for un-global banned people
-» /speedtest - run the bot server speedtest
-» /sysinfo - show the system information
-» /update - update your bot to latest version
-» /restart - restart your bot
-» /leaveall - order userbot to leave from all group
-» /leavebot (`chat id`) - order bot to leave from the group you specify
-
-» /eval - execute any code
-» /sh - run any command
-
-» /broadcast (`message`) - send a broadcast message to all groups entered by bot
-» /broadcast_pin (`message`) - send a broadcast message to all groups entered by bot with the chat pin
+» /gban (`username` یا `user id`) - مسدود کردن کلی افراد
+» /ungban (`username` یا `user id`) - حذف مسدود کلی افراد
+» /speedtest - اجرای Speddtest سرور ربات
+» /sysinfo - نمایش اطلاعات سیستم
+» /update - بروزرسانی ربات به آخرین نسخه
+» /restart - ریستارت کردن ربات
 
 ⚡ __Powered by {BOT_NAME} AI__""",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🔙 Go Back", callback_data="cbcmds")]]
-        ),
     )
 
 
@@ -184,23 +159,23 @@ async def cbsudo(_, query: CallbackQuery):
 async def cbmenu(_, query: CallbackQuery):
     a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
     if not a.can_manage_voice_chats:
-        return await query.answer("💡 Only admin with manage video chat permission that can tap this button !", show_alert=True)
+        return await query.answer("💡 فقط ادمین های با توانایی دسترسی به ویسچت قادر به استفاده از این دکمه هستند!", show_alert=True)
     chat_id = query.message.chat.id
     user_id = query.message.from_user.id
     buttons = menu_markup(user_id)
     chat = query.message.chat.title
     if chat_id in QUEUE:
           await query.edit_message_text(
-              f"⚙️ **Settings of** {chat}\n\n⏸ : pause stream\n▶️ : resume stream\n🔇 : mute userbot\n🔊 : unmute userbot\n⏹ : stop stream",
+              f"⚙️ **Settings of** {chat}\n\n⏸ : توثق پخش\n▶️ : ادامه پخش\n🔇 : میوت کردن یوزربات\n🔊 : آنمیوت کردن یوزربات\n⏹ : پایان پخش",
               reply_markup=InlineKeyboardMarkup(buttons),
           )
     else:
-        await query.answer("❌ nothing is currently streaming", show_alert=True)
+        await query.answer("❌ الان چیزی در حال پخش نیست", show_alert=True)
 
 
 @Client.on_callback_query(filters.regex("cls"))
 async def close(_, query: CallbackQuery):
     a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
     if not a.can_manage_voice_chats:
-        return await query.answer("💡 Only admin with manage video chat permission that can tap this button !", show_alert=True)
+        return await query.answer("💡 فقط ادمین های با توانایی دسترسی به ویسچت قادر به استفاده از این دکمه هستند!", show_alert=True)
     await query.message.delete()
